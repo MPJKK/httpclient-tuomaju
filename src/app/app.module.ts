8dce8a89ce0b42729f38c05be8aea0132ebfa3c2
Tuomas Juopperi <tuomaju@ety-a2123b-30.edu.wks.metropolia.fi>
2018-01-18 14:04:31 +0200

    eka

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpTestComponent} from './http-test/http-test.component';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HttpTestComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/http-test/http-test.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+    selector: 'app-http-test',
+    templateUrl: './http-test.component.html',
+    styleUrls: ['./http-test.component.scss']
+})
+export class HttpTestComponent implements OnInit {
+
+    tulos = 'moro';
+
+    constructor(private http: HttpClient) {
+    }
+
+    getJson() {
+
+        interface MyInterface {
+            license: string;
+        }
+
+        this.http.get<MyInterface>('assets/package.json').subscribe((data) => {
+            console.log(data);
+            this.tulos = data.license;
+        });
+    }
+
+    ngOnInit() {
+        this.getJson();
+    }
+
+}
